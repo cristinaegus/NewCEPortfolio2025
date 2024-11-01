8b60527db63fa2676495af15573cc6d0161da128
Marve10s <igrimanigroman@gmail.com>
2024-11-01 23:58:51 +0300

    sitemap,prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://elkamali.netlify.app'
-} // your configuration options here...
-// https://docs.astro.build/en/reference/configuration-reference/
-);
+  integrations: [tailwind(), react(), sitemap()],
+  prefetch: {
+    prefetchAll: true,
+  },
+  site: 'https://elkamali.netlify.app',
+});
