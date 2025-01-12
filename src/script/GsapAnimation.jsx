016613e8dd28ee2b87e76ff481898ade7b413faf
cristinaegus <151626172+cristinaegus@users.noreply.github.com>
2025-01-12 11:01:54 +0100

    GsapAnimation.jsx

--- src/script/GsapAnimation.jsx
+++ src/script/GsapAnimation.jsx
@@ -1,34 +1,44 @@
 import { useEffect } from 'react';
 import gsap from 'gsap';
+
 const GsapAnimation = () => {
   useEffect(() => {
-    // Animation for elements with class 'animate'
-    gsap.from('.animate', {
-      opacity: 0,
-      y: 20,
-      duration: 1,
-      stagger: 0.2,
-      ease: 'power2.out',
-    });
-    // Animation for elements with class 'floating-span'
-    const spans = document.querySelectorAll('.floating-span');
-    spans.forEach(span => {
-      gsap.to(span, {
-        y: '-5',
+    const animateElements = document.querySelectorAll('.animate');
+    if (animateElements.length) {
+      gsap.from(animateElements, {
+        opacity: 0,
+        y: 20,
         duration: 1,
-        ease: 'power2.inOut',
-        repeat: -1,
-        yoyo: true,
+        stagger: 0.2,
+        ease: 'power2.out',
       });
-    });
-    // Animation for elements with class 'animated-text'
-    gsap.to('.animated-text', {
-      opacity: 1,
-      y: 0,
-      duration: 1,
-      ease: 'power2.out',
-    });
+    }
+
+    const spans = document.querySelectorAll('.floating-span');
+    if (spans.length) {
+      spans.forEach(span => {
+        gsap.to(span, {
+          y: '-5',
+          duration: 1,
+          ease: 'power2.inOut',
+          repeat: -1,
+          yoyo: true,
+        });
+      });
+    }
+
+    const animatedText = document.querySelectorAll('.animated-text');
+    if (animatedText.length) {
+      gsap.to(animatedText, {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power2.out',
+      });
+    }
   }, []);
+
   return null;
 };
+
 export default GsapAnimation;
